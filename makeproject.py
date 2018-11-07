d2aabc3181acb7c9b084ee4bee1514693251e8b9
Adam Hosman <github@adamsinbox.com>
2018-11-07 12:15:56 +0100

    Update makeproject.py
    - add URL prompt
    - improve .py template with URL
    - update documentation
    - add TODO

--- makeproject.py
+++ makeproject.py
@@ -1,20 +1,21 @@
 """Script to automate folder & file creation for challenges
 
-- example:
-  $ python3 makeproject.py
-  Num:  33
-  Name: Kickass Fibonacci Sequence #5!
-- creates:
-  './→33_kickass-fibonacci-sequence-5/kickass-fibonacci-sequence-5.py'
+Example:
+  - $ python3 makeproject.py
+    Num:  33
+    Name: Kickass Fibonacci Sequence #5!
+    URL:  http://not-important-in-this-example.com
+  - creates `./→33_kickass-fibonacci-sequence-5/kickass-fibonacci-sequence-5.py`
 """
 
 # TODO: take URL as input to...
 # - extract challenge type (Codewars / Project Euler / other)
 # - scrape number & title from page
+# TODO: open folder in Finder / file in IDE
 
 import os
 
-def make_project(num, name):
+def make_project(num, name, url):
   os.chdir(os.sys.path[0])  # set CWD (avoid creating the files somewhere else)
   name = name.strip().lower().replace(' ', '-')
   for char in set(name):
@@ -25,13 +26,14 @@ def make_project(num, name):
   os.makedirs(dirname)
   os.chdir(dirname)
   with open(filename, 'w') as file:
-    file.write('\"\"\"URL\"\"\"\n\ndef func():\n  pass\n\n# TESTS\n')
+    file.write(f'\"\"\"{url}\"\"\"\n\ndef func():\n  pass\n\n# TESTS\n')
 
 
 def main():
   num = input('Num:  ')
   name = input('Name: ')
-  make_project(num, name)
+  url = input('URL:  ')
+  make_project(num, name, url)
 
 if __name__ == '__main__':
   main()
