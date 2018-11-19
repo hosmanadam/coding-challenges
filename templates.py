3e03df3ef47c728e06982634e7e7188f3c91d784
Adam Hosman <github@adamsinbox.com>
2018-11-20 00:04:42 +0100

    Add templates.py

---
+++ templates.py
@@ -0,0 +1,21 @@
+UNITTEST_TEMPLATE = '''\
+import unittest
+
+import main
+
+
+class SampleTests(unittest.TestCase):
+
+    def test1(self):
+        pass # TODO: write unit tests
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+'''
+
+
+FUNCTION_TEMPLATE = '''\
+def foo():
+    pass # TODO: implement
+'''
