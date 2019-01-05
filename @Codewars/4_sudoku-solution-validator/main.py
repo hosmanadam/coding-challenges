280f3ef4307e6a1cdded1048003a250157227c45
Adam Hosman <github@adamsinbox.com>
2019-01-05 15:06:45 +0100

    Update sudoku-solution-validator
    - rename coordinates (x, y) to (col, row)
    - swap stored order (col, row) to (row, col) to match board indexing order
    - add draft

--- @Codewars/4_sudoku-solution-validator/main.py
+++ @Codewars/4_sudoku-solution-validator/main.py
@@ -1,21 +1,21 @@
 """https://www.codewars.com/kata/sudoku-solution-validator/train/python"""
 
 
-ROW = {'starts': [(0, y) for y in range(9)],
-       'moves': [(x, 0) for x in range(9)]}
+ROW = {'starts': [(row, 0) for row in range(9)],
+       'moves': [(0, col) for col in range(9)]}
 
-COLUMN = {'starts': [(x, 0) for x in range(9)],
-          'moves': [(0, y) for y in range(9)]}
+COLUMN = {'starts': [(0, col) for col in range(9)],
+          'moves': [(row, 0) for row in range(9)]}
 
-SUBGRID = {'starts': [(x, y) for x in (0, 3, 6) for y in (0, 3, 6)],
-           'moves': [(x, y) for x in range(3) for y in range(3)]}
+SUBGRID = {'starts': [(row, col) for col in (0, 3, 6) for row in (0, 3, 6)],
+           'moves': [(row, col) for col in range(3) for row in range(3)]}
 
 
 def validSolution(board):
     for shape in ROW, COLUMN, SUBGRID:
         for start in shape['starts']:
             subset_coordinates = [(start[0] + move[0], start[1] + move[1]) for move in shape['moves']]
-            subset = [board[y][x] for x, y in subset_coordinates]
+            subset = [board[row][col] for row, col in subset_coordinates]
             if not all(num in subset for num in (1, 2, 3, 4, 5, 6, 7, 8, 9)):
                 return False
     return True
