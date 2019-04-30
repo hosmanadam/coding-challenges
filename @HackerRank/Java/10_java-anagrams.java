969be4f95cbead760af9600c455a43699d8478b9
Adam Hosman <github@adamsinbox.com>
2019-04-30 15:34:16 +0200

    Solve java-anagrams

---
+++ @HackerRank/Java/10_java-anagrams.java
@@ -0,0 +1,35 @@
+/* https://www.hackerrank.com/challenges/java-anagrams/problem */
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length())
+            return false;
+
+        java.util.HashMap<Character, Integer> aMap = counter(a.toLowerCase());
+        java.util.HashMap<Character, Integer> bMap = counter(b.toLowerCase());
+        return aMap.equals(bMap);
+    }
+
+    public static java.util.HashMap<Character, Integer> counter(String string) {
+        java.util.HashMap<Character, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = string.charAt(i);
+            int count = map.getOrDefault(currentChar, 0);
+            map.put(currentChar, count + 1);
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
