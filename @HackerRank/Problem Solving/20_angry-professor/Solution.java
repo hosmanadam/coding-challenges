dab60a9773786ae80434ff2907c8f0aa51de7ceb
Adam Hosman <github@adamsinbox.com>
2019-05-03 11:44:30 +0200

    Solve angry-professor (JAVA)

---
+++ @HackerRank/Problem Solving/20_angry-professor/Solution.java
@@ -0,0 +1,57 @@
+/* https://www.hackerrank.com/challenges/angry-professor/problem */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    static String angryProfessor(int threshold, int[] arrivals) {
+        boolean canceled = countOnTime(arrivals) < threshold;
+        return (canceled ? "YES" : "NO");
+    }
+
+    static int countOnTime(int[] arrivals) {
+        int count = 0;
+        for (int arrival : arrivals) {
+            if (arrival <= 0)
+                count++;
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int[] a = new int[n];
+
+            String[] aItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int aItem = Integer.parseInt(aItems[i]);
+                a[i] = aItem;
+            }
+
+            String result = angryProfessor(k, a);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
