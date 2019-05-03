bc962a7a273925b35068e9e79cbe0194514527b6
Adam Hosman <github@adamsinbox.com>
2019-05-03 14:20:06 +0200

    Solve save-the-prisoner (JAVA)

---
+++ @HackerRank/Problem Solving/15_save-the-prisoner/Solution.java
@@ -0,0 +1,41 @@
+/* https://www.hackerrank.com/challenges/save-the-prisoner/problem */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    static int saveThePrisoner(int prisoners, int candy, int start) {
+        int offset = start - 1;
+        int target = (candy + offset) % prisoners;
+        return (target != 0 ? target : prisoners);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nms = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nms[0]);
+
+            int m = Integer.parseInt(nms[1]);
+
+            int s = Integer.parseInt(nms[2]);
+
+            int result = saveThePrisoner(n, m, s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
