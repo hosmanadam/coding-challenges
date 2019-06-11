4def6b74a30711661763b0b8410059dde1fbeb1f
Adam Hosman <github@adamsinbox.com>
2019-06-11 17:38:15 +0200

    Solve running-time-of-algorithms (JAVA)

---
+++ @HackerRank/Problem Solving/30_running-time-of-algorithms/Solution.java
@@ -0,0 +1,49 @@
+/* https://www.hackerrank.com/challenges/runningtime/problem */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private static int runningTime(int[] arr) {
+        int shifts = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int val = arr[i];
+            int head = i;
+            while (--head >= 0 && arr[head] > val) {
+                arr[head + 1] = arr[head];
+                shifts++;
+            }
+            arr[head+1] = val;
+        }
+        return shifts;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
