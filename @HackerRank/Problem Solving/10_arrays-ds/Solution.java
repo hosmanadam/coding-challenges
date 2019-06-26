c0dbb119cb32547ae53edf6c722260ef08dfbad4
Adam Hosman <github@adamsinbox.com>
2019-06-26 21:08:22 +0200

    Solve arrays-ds (JAVA)

---
+++ @HackerRank/Problem Solving/10_arrays-ds/Solution.java
@@ -0,0 +1,51 @@
+/* https://www.hackerrank.com/challenges/arrays-ds/problem */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    /** T(n) = O(n), where n is the length of a */
+    private static int[] reverseArray(int[] a) {
+        int[] reversed = new int[a.length];
+        for (int i = 0; i < a.length; i++)
+            reversed[a.length-1-i] = a[i];
+
+        return reversed;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] res = reverseArray(arr);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
